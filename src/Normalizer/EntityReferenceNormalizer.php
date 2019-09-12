1a99d71eec81c415cc2460a95ea466ce66a6fbee
Medard Rebero <medard.rebero@gmail.com>
2019-09-12 23:45:41 +0200

    W10

--- src/Normalizer/EntityReferenceNormalizer.php
+++ src/Normalizer/EntityReferenceNormalizer.php
@@ -4,6 +4,7 @@
 namespace Drupal\hir_rest_resources\Normalizer;
 
 
+use Drupal;
 use Drupal\serialization\Normalizer\EntityReferenceFieldItemNormalizer;
 
 class EntityReferenceNormalizer extends EntityReferenceFieldItemNormalizer
@@ -11,7 +12,7 @@ class EntityReferenceNormalizer extends EntityReferenceFieldItemNormalizer
 
     public function normalize($field_item, $format = NULL, array $context = [])
     {
-        \Drupal::logger('hir_rest_resources')->info(json_encode($field_item));
+        Drupal::logger('hir_rest_resources')->info(json_encode($field_item));
         return parent::normalize($field_item, $format, $context);
     }
 }
