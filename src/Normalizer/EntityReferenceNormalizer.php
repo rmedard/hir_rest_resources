8505aacddb78bad0b4ae8838755c5c6c0e9da5ce
Medard Rebero <medard.rebero@gmail.com>
2019-10-01 01:35:08 +0200

    W10

--- src/Normalizer/EntityReferenceNormalizer.php
+++ src/Normalizer/EntityReferenceNormalizer.php
@@ -13,8 +13,10 @@ class EntityReferenceNormalizer extends EntityReferenceFieldItemNormalizer
     public function normalize($field_item, $format = NULL, array $context = [])
     {
         $attributes = parent::normalize($field_item);
-        Drupal::logger('hir_rest_resources')
-            ->info('<pre><code>' . print_r($attributes, TRUE) . '</code></pre>');
+        if ($attributes['target_id'] and $attributes['width'] and $attributes['height']) {
+            Drupal::logger('hir_rest_resources')
+                ->info('<pre><code>' . print_r($attributes, TRUE) . '</code></pre>');
+        }
 //        if ($field_item instanceof FileEn)
         return $attributes;
     }
