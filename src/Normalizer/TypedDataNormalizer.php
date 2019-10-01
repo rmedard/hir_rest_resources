16bc8384ba6ff6414678556275c0bdaa7d75a444
Medard Rebero <medard.rebero@gmail.com>
2019-10-01 03:05:40 +0200

    W10

--- src/Normalizer/TypedDataNormalizer.php
+++ src/Normalizer/TypedDataNormalizer.php
@@ -35,16 +35,16 @@ class TypedDataNormalizer extends NormalizerBase
                     $value['file_url'] = file_create_url(File::load($value['target_id'])->getFileUri());
                 }
             }
-//            if (is_array($values) and isset($values[0])) {
-//                if (isset($values[0]['value'])) {
-//                    $values = $values[0]['value'];
-//                }
+            if (is_array($values) and isset($values[0])) {
+                if (isset($values[0]['value'])) {
+                    $values = $values[0]['value'];
+                }
 //                if (isset($values[0]['target_id']) and isset($values[0]['width']) and isset($values[0]['height'])) {
 //                    for ($i = 0; $i < $values->length; $i++) {
 //                        $values[$i]['file_url'] = file_create_url(File::load($values[$i]['target_id'])->getFileUri());
 //                    }
 //                }
-//            }
+            }
         }
         return $values;
     }
