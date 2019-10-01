5ff8c96dbefc20e431a95407436f102a28457ec0
Medard Rebero <medard.rebero@gmail.com>
2019-10-01 03:03:26 +0200

    W10

--- src/Normalizer/TypedDataNormalizer.php
+++ src/Normalizer/TypedDataNormalizer.php
@@ -12,6 +12,7 @@ namespace Drupal\hir_rest_resources\Normalizer;
 use Drupal;
 use Drupal\Core\TypedData\TypedDataInterface;
 use Drupal\file\Entity\File;
+use Drupal\file\Plugin\Field\FieldType\FileFieldItemList;
 use Drupal\serialization\Normalizer\NormalizerBase;
 
 class TypedDataNormalizer extends NormalizerBase
@@ -28,27 +29,22 @@ class TypedDataNormalizer extends NormalizerBase
         $values = array();
         if ($object instanceof TypedDataInterface) {
             $values = $object->getValue();
-            if ($object->getName() === 'field_advert_picture') {
-                Drupal::logger('hir_rest_resources')
-                    ->info("Type: " . $object->getDataDefinition()->getClass());
-                $data = $object->getValue();
-//                if ($data instanceof Drupal\file\Plugin\Field\FieldType\FileFieldItemList) {
-//                    $data->getValue()
-//
+            if ($values instanceof FileFieldItemList) {
+                Drupal::logger('hir_rest_resources')->info("Item list found...");
+                foreach ($values as $value) {
+                    $value['file_url'] = file_create_url(File::load($value['target_id'])->getFileUri());
+                }
+            }
+//            if (is_array($values) and isset($values[0])) {
+//                if (isset($values[0]['value'])) {
+//                    $values = $values[0]['value'];
 //                }
-                kint($object);
-                die();
-            }
-            if (is_array($values) and isset($values[0])) {
-                if (isset($values[0]['value'])) {
-                    $values = $values[0]['value'];
-                }
-                if (isset($values[0]['target_id']) and isset($values[0]['width']) and isset($values[0]['height'])) {
-                    for ($i = 0; $i < $values->length; $i++) {
-                        $values[$i]['file_url'] = file_create_url(File::load($values[$i]['target_id'])->getFileUri());
-                    }
-                }
-            }
+//                if (isset($values[0]['target_id']) and isset($values[0]['width']) and isset($values[0]['height'])) {
+//                    for ($i = 0; $i < $values->length; $i++) {
+//                        $values[$i]['file_url'] = file_create_url(File::load($values[$i]['target_id'])->getFileUri());
+//                    }
+//                }
+//            }
         }
         return $values;
     }
