6bfbcfe73311077268f7a3e4607a55ce99a7088a
Medard Rebero <medard.rebero@gmail.com>
2019-10-10 00:08:49 +0200

    W10

--- src/Service/CouchDbService.php
+++ src/Service/CouchDbService.php
@@ -50,7 +50,8 @@ class CouchDbService
     public function createEntity($entityArray) {
         try {
             $picture = Drupal\file\Entity\File::load(17444);
-            $attachment = Attachment::createFromBase64Data(base64_encode($picture), 'image/jpeg');
+            $url = 'http://www.hir-dev.ml/sites/default/files/2019-02/1kagugu-plot-plut-properties-3.jpg';
+            $attachment = Attachment::createFromBase64Data(base64_encode(file_get_contents($url)), 'image/jpeg');
             $entityArray['attachments'] = array('firstImage.jpg' => $attachment);
             return $this->client->postDocument($entityArray);
         } catch (HTTPException $e) {
